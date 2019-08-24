536b7adf5adea620f5e56268f9b016e6be424e9a
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-24 21:32:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,26 @@ class Triangle
   end
   
   def is_valid?(array)
-    i = 0
     
+   
+    return false if array.include?(0)
+    
+    firs_element = array.first
+    
+    sum_1 = array.first + array[1]
+    sum_2 = array.first + array.last 
+    sum_3 = array[1] + array.last
+   
     while i < array.size do 
-      array[i]
+      firs_element + array[i] 
+
+       
+     
+      i += 1
+      
     end
+    
+    
   end
   
   def kind
