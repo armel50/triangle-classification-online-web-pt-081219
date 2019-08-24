54023b0efe82ece400b00da7dc912a13d384660d
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-24 20:38:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_accessor :sides, :kind
+  def initialize(side1, side2, side3)
+    @sides = []
+    @sides << side1 
+    @sides << side2 
+    @sides << side3 
+    
+  end
+  
+  def kind
+    if (@side[0] < 0 || @side[1] < 0 || @side[2] < 0)
+      
+        raise TriangleError
+    
+    end
+    
+  end
+  
+    class TriangleError < StandardError
+      
+    end
+  
 end
